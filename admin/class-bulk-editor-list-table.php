347aa51d70d1a662ef4fe8cd33324f9a3de8b194
Joost de Valk <joost@yoast.com>
2014-09-18 19:20:54 +0200

    More code style. Sigh.

--- admin/class-bulk-editor-list-table.php
+++ admin/class-bulk-editor-list-table.php
@@ -205,7 +205,7 @@ if ( ! class_exists( 'WPSEO_Bulk_List_Table' ) ) {
 			?>
 			<div class="tablenav <?php echo esc_attr( $which ); ?>">
 
-				<?php if ('top' === $which ) { ?>
+				<?php if ( 'top' === $which ) { ?>
 				<form id="posts-filter" action="" method="get">
 					<input type="hidden" name="page" value="wpseo_bulk-editor" />
 					<input type="hidden" name="type" value="<?php echo $this->page_type; ?>" />
@@ -223,7 +223,7 @@ if ( ! class_exists( 'WPSEO_Bulk_List_Table' ) ) {
 					?>
 
 					<br class="clear" />
-					<?php if ('top' === $which) { ?>
+					<?php if ( 'top' === $which ) { ?>
 				</form>
 			<?php } ?>
 			</div>
